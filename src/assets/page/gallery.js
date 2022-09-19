10cadd4a463d030b7981a80685157465212dfa58
NiftyBuffalo619 <polaclukes@outlook.cz>
2022-09-19 19:34:14 +0200

    Improved Cloud🗂

--- src/assets/page/gallery.js
+++ src/assets/page/gallery.js
@@ -1,1 +1,16 @@
-import * as dropbox from 'dropbox';
+const dropbox = require('dropbox');
+const { Dropbox } = require('dropbox');
+const dotenv = require('dotenv').config();
+const dbx = new Dropbox({
+    accessToken: process.env.token
+});
+dbx.usersGetCurrentAccount().then(function (response) {
+    console.log(response);
+}).catch(function (err) {
+console.error(err);
+});
+dbx.filesListFolder({path: ''}).then(function (response) {
+    console.log(response);
+}).catch(function (err) {
+console.error(err);
+});
